454de6ed289a2e6a9cd48dfe19d596a3f9ac005d
anxi.xue <anxi.xue@163.com>
2018-03-12 23:05:07 +0800

    Add skill test:huo gong

--- AshesOfHistory/app/src/androidTest/java/com/game/xianxue/ashesofhistory/databaseTest/SkillTest.java
+++ AshesOfHistory/app/src/androidTest/java/com/game/xianxue/ashesofhistory/databaseTest/SkillTest.java
@@ -1,10 +1,20 @@
 package com.game.xianxue.ashesofhistory.databaseTest;
 
 import android.content.Context;
+import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.game.xianxue.ashesofhistory.database.LineUpDataManager;
+import com.game.xianxue.ashesofhistory.database.PersonDataManager;
 import com.game.xianxue.ashesofhistory.database.SkillDataManager;
+import com.game.xianxue.ashesofhistory.game.engine.BattleEngine;
+import com.game.xianxue.ashesofhistory.game.model.TeamModel;
+import com.game.xianxue.ashesofhistory.game.model.lineup.LineUpBase;
+import com.game.xianxue.ashesofhistory.game.model.lineup.LineUpBattle;
+import com.game.xianxue.ashesofhistory.game.model.person.BasePerson;
+import com.game.xianxue.ashesofhistory.game.model.person.BattlePerson;
+import com.game.xianxue.ashesofhistory.game.model.person.NormalPerson;
 import com.game.xianxue.ashesofhistory.game.skill.SkillBase;
 import com.game.xianxue.ashesofhistory.utils.ShowUtils;
 import com.game.xianxue.ashesofhistory.utils.XmlUtils;
@@ -72,6 +82,52 @@ public class SkillTest {
         init();
         SkillDataManager.getSkillFromDataBaseById(0);
     }
+    /**
+     * 测试 火攻 技能
+     */
+    @Test
+    public void TestHuoGong() {
+        init();
+        BasePerson play1 = PersonDataManager.getPersonFromDataBaseByPinyin("zhugeliang");
+        BattlePerson b1 = new BattlePerson(new NormalPerson(play1,15));
+        BasePerson play2 = PersonDataManager.getPersonFromDataBaseByPinyin("liubei");
+        BattlePerson b2 = new BattlePerson(new NormalPerson(play2,15));
+        b2.setLeader(true);
+        b1.setCounsellor(true);
+        ArrayList<BattlePerson> playerList1 = new ArrayList<BattlePerson>();
+        playerList1.add(b1);
+        playerList1.add(b2);
 
 
+        BasePerson play3= PersonDataManager.getPersonFromDataBaseByPinyin("xvchu");
+        BattlePerson b3 = new BattlePerson(new NormalPerson(play3,15));
+        BasePerson play4 = PersonDataManager.getPersonFromDataBaseByPinyin("caocao");
+        BattlePerson b4 = new BattlePerson(new NormalPerson(play4,15));
+        b4.setLeader(true);
+        b3.setCounsellor(true);
+        ArrayList<BattlePerson> playerList2 = new ArrayList<BattlePerson>();
+        playerList2.add(b3);
+        playerList2.add(b4);
+
+        // 初始化阵型1
+        LineUpBase lineUp1 = LineUpDataManager.getDataFromDataBaseById(0);//普通阵容
+        LineUpBattle lb1 = new LineUpBattle(lineUp1,playerList1);
+        lb1.displayMatrix();
+
+        // 初始化阵型2
+        LineUpBase lineUp2 = LineUpDataManager.getDataFromDataBaseById(0);//普通阵容
+        LineUpBattle lb2 = new LineUpBattle(lineUp2,playerList2);
+        lb2.displayMatrix();
+
+        // 初始化阵营
+        TeamModel t1 = new TeamModel(TeamModel.CAMP_LEFT, lb1);
+        TeamModel t2 = new TeamModel(TeamModel.CAMP_RIGHT, lb2);
+
+        BattleEngine engine = BattleEngine.getInstance();
+        engine.setBattleTeam(t1, t2);
+        engine.setTimeActiveIncrese(50);
+        engine.setTimePersonAction(50);
+        engine.startBattle();
+        SystemClock.sleep(999999);
+    }
 }
